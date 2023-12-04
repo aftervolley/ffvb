d865502c6caa1a2584b6d201314740a41d5d682e
Sylvain Lesage <severo@rednegra.net>
2023-12-04 11:39:59 +0100

    add test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import { add } from '../src/index.js';
+
+describe('index.js', function () {
+  describe('add()', function () {
+    it('should return 3 when adding 1 and 2', function () {
+      assert.equal(add(1,2), 3);
+    });
+  });
+});
