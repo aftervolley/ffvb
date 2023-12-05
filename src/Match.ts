1ad7489b80c148f73d848485512e89240967f295
Sylvain Lesage <severo@rednegra.net>
2023-12-05 11:51:22 +0100

    remove dead code and refactor

--- src/Match.ts
+++ src/Match.ts
@@ -5,6 +5,17 @@ import { Team } from "./Team";
 import { Result } from "./Result";
 import { ScoringSet } from "./ScoringSet";
 
+const TEAM_WIDTH = 155.91;
+const TEAM_WIDTH_SET_5 = 136.06;
+const X_TEAM1 = 0;
+const X_SET_1_3 = 142.07;
+const X_SET_2_4 = X_SET_1_3 + 334.49;
+const X_SET_5 = 42.86;
+const X_TEAM2 = 127.56;
+const Y_LINE_1 = 0;
+const Y_LINE_2 = -93.54;
+const Y_LINE_3 = -187.08;
+
 export class Match {
   parser: PDFParser;
   metadata: Metadata;
@@ -13,19 +24,9 @@ export class Match {
   result: Result;
   sets: ScoringSet[];
 
-  static async fromPage(page: PDFPageProxy) {
+  static async fromPage(page: PDFPageProxy): Promise<Match> {
     const match = new Match();
     await match.parser.parsePage(page);
-
-    // function viewPdf(page: PDFPageProxy) {
-    //   const scale = width / page.getViewport({ scale: 1 }).width;
-    //   const viewport = page.getViewport({ scale });
-    //   const context = DOM.context2d(viewport.width, viewport.height);
-    //   context.canvas.style.border = "solid 1px #ccc";
-    //   page.render({ canvasContext: context, viewport });
-    //   return html`<p>${context.canvas}</p>`;
-    // }
-    // match.view = () => viewPdf(page);
     return match;
   }
 
@@ -33,75 +34,74 @@ export class Match {
     const parser = new PDFParser();
     this.parser = parser;
     this.metadata = new Metadata({ parser });
-    this.team1 = new Team({ parser, xOffset: 0 });
-    this.team2 = new Team({ parser, xOffset: 127.56 });
+    this.team1 = new Team({ parser, xOffset: X_TEAM1 });
+    this.team2 = new Team({ parser, xOffset: X_TEAM2 });
     this.result = new Result({ parser });
-    const DEFAULT_SET_XOFFSET = 142.06600000000003;
-    const DEFAULT_TEAM_WIDTH = 155.91000000000003;
+
     this.sets = [
       new ScoringSet({
         parser,
         setNumber: 1,
-        yOffset: 0,
+        yOffset: Y_LINE_1,
         teamLeftSizes: {
-          xOffset: DEFAULT_SET_XOFFSET,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_1_3,
+          width: TEAM_WIDTH,
         },
         teamRightSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + DEFAULT_TEAM_WIDTH,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_1_3 + TEAM_WIDTH,
+          width: TEAM_WIDTH,
         },
       }),
       new ScoringSet({
         parser,
         setNumber: 2,
-        yOffset: 0,
+        yOffset: Y_LINE_1,
         teamLeftSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + 334.49,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_2_4,
+          width: TEAM_WIDTH,
         },
         teamRightSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + 334.49 + DEFAULT_TEAM_WIDTH,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_2_4 + TEAM_WIDTH,
+          width: TEAM_WIDTH,
         },
       }),
       new ScoringSet({
         parser,
         setNumber: 3,
-        yOffset: -93.54000000000008,
+        yOffset: Y_LINE_2,
         teamLeftSizes: {
-          xOffset: DEFAULT_SET_XOFFSET,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_1_3,
+          width: TEAM_WIDTH,
         },
         teamRightSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + DEFAULT_TEAM_WIDTH,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_1_3 + TEAM_WIDTH,
+          width: TEAM_WIDTH,
         },
       }),
       new ScoringSet({
         parser,
         setNumber: 4,
-        yOffset: -93.54000000000008,
+        yOffset: Y_LINE_2,
         teamLeftSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + 334.49,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_2_4,
+          width: TEAM_WIDTH,
         },
         teamRightSizes: {
-          xOffset: DEFAULT_SET_XOFFSET + 334.49 + DEFAULT_TEAM_WIDTH,
-          width: DEFAULT_TEAM_WIDTH,
+          xOffset: X_SET_2_4 + TEAM_WIDTH,
+          width: TEAM_WIDTH,
         },
       }),
       new ScoringSet({
         parser,
         setNumber: 5,
-        yOffset: -187.08000000000004,
+        yOffset: Y_LINE_3,
         teamLeftSizes: {
-          xOffset: 42.855999999999995,
-          width: 136.06000000000006,
+          xOffset: X_SET_5,
+          width: TEAM_WIDTH_SET_5,
         },
         teamRightSizes: {
-          xOffset: 42.855999999999995 + 136.06000000000006,
-          width: 136.06000000000006,
+          xOffset: X_SET_5 + TEAM_WIDTH_SET_5,
+          width: TEAM_WIDTH_SET_5,
         },
       }),
     ];
